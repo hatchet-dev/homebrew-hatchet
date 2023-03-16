3bfd23bd367342afa2c004e7f3dde223e38f4b7a
Alexander Belanger <belanger@sas.upenn.edu>
2023-03-16 09:10:09 -0400

    remove equals sign

--- Formula/hatchet-server.rb
+++ Formula/hatchet-server.rb
@@ -2,7 +2,7 @@ class HatchetServer < Formula
     homepage "https://hatchet.run"
     version "v0.1.0-alpha.0"
 
-    url = "https://github.com/hatchet-dev/hatchet/releases/download/v0.1.0-alpha.0/hatchet-server_v0.1.0-alpha.0_Darwin_x86_64.zip" 
+    url "https://github.com/hatchet-dev/hatchet/releases/download/v0.1.0-alpha.0/hatchet-server_v0.1.0-alpha.0_Darwin_x86_64.zip" 
     sha256 "5246fc24bd5cbd5d6431e65791860c2c5bf775949c456a8cad3f681c4414f486"
 
     on_macos do
--- Formula/hatchet.rb
+++ Formula/hatchet.rb
@@ -5,7 +5,7 @@ class Hatchet < Formula
     homepage "https://hatchet.run"
     version "v0.1.0-alpha.0"
 
-    url = "https://github.com/hatchet-dev/hatchet/releases/download/v0.1.0-alpha.0/hatchet_v0.1.0-alpha.0_Darwin_x86_64.zip" 
+    url "https://github.com/hatchet-dev/hatchet/releases/download/v0.1.0-alpha.0/hatchet_v0.1.0-alpha.0_Darwin_x86_64.zip" 
     sha256 "65e06739cbb40bf74d60af4596af35303136388c50cfe0fbe9a28c6243f12578"
 
     on_macos do
