2d49624c34b3839c0adf9b2ed28b27c36ba0a491
Alexander Belanger <belanger@sas.upenn.edu>
2023-03-16 13:03:45 -0400

    overwrite with correct shas

--- Formula/hatchet-server.rb
+++ Formula/hatchet-server.rb
@@ -3,7 +3,7 @@ class HatchetServer < Formula
     version "v0.1.0-alpha.0"
 
     url "https://github.com/hatchet-dev/hatchet/releases/download/v0.1.0-alpha.0/hatchet-server_v0.1.0-alpha.0_Darwin_x86_64.zip" 
-    sha256 "SHA25609f20bd5a566f8f2bca47351e513d8d7fb50308757d9f3d7fbdfc72ecb3fead7"
+    sha256 "09f20bd5a566f8f2bca47351e513d8d7fb50308757d9f3d7fbdfc72ecb3fead7"
           
     on_macos do
       def install
--- Formula/hatchet.rb
+++ Formula/hatchet.rb
@@ -6,7 +6,7 @@ class Hatchet < Formula
     version "v0.1.0-alpha.0"
 
     url "https://github.com/hatchet-dev/hatchet/releases/download/v0.1.0-alpha.0/hatchet_v0.1.0-alpha.0_Darwin_x86_64.zip" 
-    sha256 "SHA25633bfd956999579b8b4a21363b9aea26ec010e5e369f64056e73ed6bd490159aa"
+    sha256 "33bfd956999579b8b4a21363b9aea26ec010e5e369f64056e73ed6bd490159aa"
           
     on_macos do
       def install
